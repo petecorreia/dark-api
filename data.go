d5df23c4ae2b5b550e2fb5779a5ddb66fd66c947
Pete Correia <pete@petecorreia.com>
2020-11-06 15:03:33 +0000

    Get port from PORT env var

--- data.go
+++ data.go
@@ -1,16 +1,5 @@
 package main
 
-type Character struct {
-	ID            string   `json:"id"`
-	Name          string   `json:"name"`
-	Worlds        []string `json:"worlds"`
-	Aliases       []string `json:"aliases"`
-	Alternates    []string `json:"alternates"`
-	Parents       []string `json:"parents"`
-	Children      []string `json:"children"`
-	Relationships []string `json:"relationships"`
-}
-
 var Characters = []Character{
 	{
 		ID:            "jonas-kahnwald",
--- main.go
+++ main.go
@@ -1,10 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo/v4"
 )
+
+type Character struct {
+	ID            string   `json:"id"`
+	Name          string   `json:"name"`
+	Worlds        []string `json:"worlds"`
+	Aliases       []string `json:"aliases"`
+	Alternates    []string `json:"alternates"`
+	Parents       []string `json:"parents"`
+	Children      []string `json:"children"`
+	Relationships []string `json:"relationships"`
+}
 
 func IndexHandler(c echo.Context) error {
 	return c.String(http.StatusOK, "Dark API")
@@ -34,6 +47,11 @@ func CharacterHandler(c echo.Context) error {
 }
 
 func main() {
+	port, exists := os.LookupEnv("PORT")
+	if !exists {
+		port = "8000"
+	}
+
 	e := echo.New()
 
 	e.GET("/", IndexHandler)
@@ -41,5 +59,5 @@ func main() {
 	e.GET("/characters", CharactersHandler)
 	e.GET("/characters/:id", CharacterHandler)
 
-	e.Logger.Fatal(e.Start(":8000"))
+	e.Logger.Fatal(e.Start(fmt.Sprintf(":%s", port)))
 }
