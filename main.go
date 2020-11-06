0912b7439bf0c589e3cb640365fb7e79f3ad9530
Pete Correia <pete@petecorreia.com>
2020-11-06 16:37:10 +0000

    Display helpful text at the index route

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+// Character represents one of the Dark characters
 type Character struct {
 	ID            string   `json:"id"`
 	Name          string   `json:"name"`
@@ -20,14 +21,23 @@ type Character struct {
 	Relationships []string `json:"relationships"`
 }
 
+// IndexHandler handles the index route and displays a helpful list of the
+// available endpoints
 func IndexHandler(c echo.Context) error {
-	return c.String(http.StatusOK, "Dark API")
+	const info = `Dark API
+
+	/characters         gets all characters
+	/characters/:id     get a specific character`
+
+	return c.String(http.StatusOK, info)
 }
 
+// CharactersHandler returns all character information
 func CharactersHandler(c echo.Context) error {
 	return c.JSON(http.StatusOK, Characters)
 }
 
+// CharacterHandler returns information about a specific character
 func CharacterHandler(c echo.Context) error {
 	id := c.Param("id")
 
